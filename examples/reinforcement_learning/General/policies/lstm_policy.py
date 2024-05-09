780ad6bf9ecb7d5fa3ac43cf9183bd1cd5e9a522
Tim Faust <faust.tim@web.de>
2024-05-09 19:49:38 +0200

    refactor lstm net

--- examples/reinforcement_learning/General/policies/lstm_policy.py
+++ examples/reinforcement_learning/General/policies/lstm_policy.py
@@ -1,11 +1,7 @@
-import math
-from typing import List, Tuple, Optional, Dict
+from typing import List, Tuple, Dict
 
 import numpy as np
-from stable_baselines3.common.policies import ContinuousCritic
-from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from stable_baselines3.common.type_aliases import PyTorchObs
-from stable_baselines3.sac.policies import Actor
 from torch import nn
 import torch as th
 from examples.reinforcement_learning.General.environments import GeneralEnv
@@ -16,43 +12,24 @@ class LSTMModule(nn.Module):
     def __init__(self, translator):
         super().__init__()
         self.input_features = translator.observation_dim
-        self.reshape_to_lstm_input = Reshape(translator.timesteps, self.input_features)
+        self.timesteps = translator.timesteps
+        self.observation_dim = translator.observation_dim
+
         self.lstm = nn.LSTM(
             input_size=self.input_features,
             hidden_size=translator.lstm_hidden_dim,
             num_layers=translator.num_layers,
             batch_first=True
         )
-        self.extract_last_timestep = ExtractLastTimestep()
         self.action_head = nn.Linear(translator.lstm_hidden_dim, translator.lstm_output_dim)
 
-        self.lstm_net = nn.Sequential(
-            self.reshape_to_lstm_input,
-            self.lstm,
-            self.extract_last_timestep,
-            self.action_head
-        )
-
-    def extract_features(self, obs: PyTorchObs) -> th.Tensor:
-        lstm_features = self.lstm_net(obs)
-        original_features = obs[:, -self.input_features:]
-        combined_features = th.cat((original_features, lstm_features), dim=1)
+    def forward(self, obs: PyTorchObs) -> th.Tensor:
+        obs_reshaped = obs.view(-1, self.timesteps, self.observation_dim)
+        lstm_output = self.lstm(obs_reshaped)
+        lstm_last_timestep = lstm_output[0][:, -1, :]
+        original_features = obs_reshaped[:, -1, :]
+        combined_features = th.cat((original_features, lstm_last_timestep), dim=1)
         return combined_features
-
-
-class Reshape(nn.Module):
-    def __init__(self, timesteps, features_per_timestep):
-        super().__init__()
-        self.timesteps = timesteps
-        self.features_per_timestep = features_per_timestep
-
-    def forward(self, x):
-        return x.view(-1, self.timesteps, self.features_per_timestep)
-
-
-class ExtractLastTimestep(nn.Module):
-    def forward(self, x):
-        return x[0][:, -1, :]
 
 
 class LSTMTranslator(DefaultTranslator):
@@ -94,7 +71,7 @@ class LSTMActor(DefaultActor):
         return LSTMTranslator()
 
     def get_action_dist_params(self, obs: PyTorchObs) -> Tuple[th.Tensor, th.Tensor, Dict[str, th.Tensor]]:
-        features = self.lstm_net.extract_features(obs)
+        features = self.lstm_net(obs)
         return super().get_action_dist_params(features)
 
 
@@ -105,7 +82,7 @@ class LSTMCritic(DefaultCritic):
         self.lstm_net = None
 
     def forward(self, obs: th.Tensor, actions: th.Tensor) -> Tuple[th.Tensor, ...]:
-        features = self.lstm_net.extract_features(obs)
+        features = self.lstm_net(obs)
         return super().forward(features, actions)
 
 
