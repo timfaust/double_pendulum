99efbe69c95bd59ee16e3b198c1f1e81b91887ce
Tim Faust <faust.tim@web.de>
2024-07-04 12:05:24 +0200

    share feature extractor

--- examples/reinforcement_learning/General/override_sb3/sequence_policy.py
+++ examples/reinforcement_learning/General/override_sb3/sequence_policy.py
@@ -155,6 +155,7 @@ class SequenceSACPolicy(CustomPolicy):
         kwargs.update(dict(
             features_extractor_class=LSTMExtractor,
             features_extractor_kwargs=dict(translator=self.translator),
+            share_features_extractor=True
         ))
 
         super().__init__(*args, **kwargs)
